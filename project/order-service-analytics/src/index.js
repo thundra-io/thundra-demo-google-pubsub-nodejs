2d62141b1897b63f0a5634587907f7747f729895
bcaglayan <batuhan@thundra.io>
2021-12-14 15:25:49 +0000

    Remove redundant codes

--- project/order-service-analytics/src/index.js
+++ project/order-service-analytics/src/index.js
@@ -5,11 +5,6 @@ const {
 const PubSub = require('./helper/pubsub');
 const OrderCancellationPuller = require('./service/order-analytic-puller');
 
-process.env['THUNDRA_AGENT_APPLICATION_NAME'] = process.env.npm_package_name !== undefined 
-    ? process.env.npm_package_name !== undefined : 'Order-Service-Analytics';
-process.env['THUNDRA_AGENT_REPORT_REST_BASEURL'] = 'https://collector.thundra.us/v1';
-process.env['THUNDRA_APIKEY'] = '7fe9ea2e-fd4a-4862-aa30-33e7ed49833e'
-
 const thundra = require('@thundra/core');
 thundra.init();
 
